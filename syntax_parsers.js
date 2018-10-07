8bc9db35a71bd52e2dc2095984f91eeda4d30325
April Copes <antdevelopment1@gmail.com>
2018-10-07 19:30:47 -0400

    Covers topics about syntax parsers

---
+++ syntax_parsers.js
@@ -0,0 +1,2 @@
+// The syntax parser makes assumptions based on what it expects next
+// Based on this it can make some changes that may not be expected such as the keyword return. It expects a semi colon at the end
