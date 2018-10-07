03dd835dfff61bd210454d734dba2df994ea633d
April Copes <antdevelopment1@gmail.com>
2018-10-07 13:43:22 -0400

    Coverint topics around faking namespaces by using objects as containers

---
+++ faking_namespaces.js
@@ -0,0 +1,20 @@
+var greet = 'Hello!';
+var greet = 'Hola';
+
+console.log(greet);
+
+// Object created as a container for properties and methods that we want to use to avoid name collisions. 
+var english = {};
+var spanish = {};
+
+english.greet = 'Hello!';
+spanish.greet = 'Hola!'
+
+console.log(english.greet)
+
+// This line erros because greetings does not exsist. We have left to right associativity and english.greet is undefined.
+// So when we call .greet as well we are saying undefined.greet which results in an error. We cannot set the property greet 
+// of undefined. Cool right?
+english.greetings = {}; // Now we can call on the line below.
+english.greetings.greet = 'Hello!';
+console.log(english);
