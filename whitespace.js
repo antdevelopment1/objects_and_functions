b8f7ee0297e1a11390133cd0876f800e5e5628f7
April Copes <antdevelopment1@gmail.com>
2018-10-07 19:43:51 -0400

    examples of excesive whitespace

---
+++ whitespace.js
@@ -0,0 +1,19 @@
+var firstname, lastname, language;
+
+var person = {
+
+// This is a comment
+    firstname: 'John',
+
+
+    // This is another
+    lastname: 
+    // and another
+    'Doe'
+};
+
+console.log(
+    // and another
+    person)
+
+// I wont want to do it but this is just to show how liberal javascript is with whitespace
