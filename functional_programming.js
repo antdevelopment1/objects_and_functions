c1985b33c6fee78361269390208aaed51d5bcda3
April Copes <antdevelopment1@gmail.com>
2018-10-12 23:58:59 -0400

    Examples of functional programming map from scratch

--- functional_programming.js
+++ functional_programming.js
@@ -1,30 +1,30 @@
-function mapForEach(arr, fn) {
-    var newArr = [];
-    for (var i = 0; i < arr.length; i++) {
+// function mapForEach(arr, fn) {
+//     var newArr = [];
+//     for (var i = 0; i < arr.length; i++) {
 
-        newArr.push(fn(arr[i]));
+//         newArr.push(fn(arr[i]));
     
-    };
-    return newArr;
+//     };
+//     return newArr;
 
-}
+// }
 
-var arr1 = [1, 2, 3];
-console.log(arr1);
-var newArr = arr1.map(function(array1) {
-    return array1 * 2;
-})
-// _.map(arr1, function (blue) {
-//   return blue * 2;
+// var arr1 = [1, 2, 3];
+// console.log(arr1);
+// var newArr = arr1.map(function(array1) {
+//     return array1 * 2;
 // })
-console.log(newArr);
+// // _.map(arr1, function (blue) {
+// //   return blue * 2;
+// // })
+// console.log(newArr);
 
-var arr2 = mapForEach(arr1, function(item) {
-    return item * 2;
-});
+// var arr2 = mapForEach(arr1, function(item) {
+//     return item * 2;
+// });
  
 
-console.log(arr2);
+// console.log(arr2);
 
 // Map function that loops through each element
 
@@ -42,8 +42,29 @@ var list = [1, 2, 3];
 
 
 // Call the function that will take the function that i want to change my list as an argument
-modifiedArray = mapThis(list, function(val) {
+var modifiedArray = mapThis(list, function(val) {
     return val * 10;
 })
 
 console.log(modifiedArray);
+
+var arr3 = mapThis(list, function(val) {
+    return val > 2;
+})
+console.log(arr3);
+var checkPastLimit = function(limiter, val) {
+    return val > limiter;
+}
+var arr4 = mapThis(list, checkPastLimit.bind(this, 1));
+console.log(arr4);
+
+
+var checkPastLimitSimplified = function(limiter) {
+    return function(limiter, item) {
+        return item > limiter;
+    }.bind(this, limiter);
+}
+
+var arr5 = mapThis(list, checkPastLimitSimplified(2));
+console.log(arr5);
+
